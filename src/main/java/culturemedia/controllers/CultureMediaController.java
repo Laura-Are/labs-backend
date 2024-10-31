f813a08bb250444641f5bd38330d1fa06772010e
Laura <lauraareizahenao@gmail.com>
2024-10-30 19:26:48 -0500

    Update Adding-controller based on peer code review feedback.

--- src/main/java/culturemedia/controllers/CultureMediaController.java
+++ src/main/java/culturemedia/controllers/CultureMediaController.java
@@ -1,26 +1,22 @@
 package culturemedia.controllers;
 
-import java.util.*;
+import java.util.List;
 
 import culturemedia.exception.VideoNotFoundException;
 import culturemedia.model.Video;
-import culturemedia.service.impl.CulturemediaServiceImpl;
+import culturemedia.service.CulturemediaService;
 
 public class CultureMediaController {
 
-    private final CulturemediaServiceImpl cultureMediaService;
-
-
-    public CultureMediaController(CulturemediaServiceImpl cultureMediaService) {
+    private final CulturemediaService cultureMediaService;
+    
+    public CultureMediaController(CulturemediaService cultureMediaService) {
         this.cultureMediaService = cultureMediaService;
     }
 
-
-    public List<Video> find_allVideos() throws VideoNotFoundException {
-        List<Video> videos = null;
+    public List<Video> findAllVideos() throws VideoNotFoundException {
+        List<Video> videos;
         videos = cultureMediaService.findAll();
         return videos;
     }
-
-
 }
